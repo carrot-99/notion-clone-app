4b9a86c9461143250612b06fadc7dc399286c6aa
carrot-99 <kaito.01204@gmail.com>
2023-01-29 21:19:03 +0900

    update section8

--- react-docker/src/pages/Memo.jsx
+++ react-docker/src/pages/Memo.jsx
@@ -9,11 +9,13 @@ import memoApi from '../api/memoApi';
 import { useState } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { setMemo } from '../redux/features/memoSlice';
+import EmojiPicker from '../components/common/EmojiPicker';
 
 const Memo = () => {
 const {memoId} =useParams();
 const [title, setTitle] = useState("");
 const [description, setDescription] = useState("");
+const [icon, setIcon] = useState("");
 const memos = useSelector((state) => state.memo.value);
 const dispatch = useDispatch();
 const navigate = useNavigate();
@@ -25,6 +27,7 @@ const navigate = useNavigate();
         // console.log(res.title);
         setTitle(res.title);
         setDescription(res.description);
+        setIcon(res.icon);
       } catch (err) {
         alert(err);
       }
@@ -84,6 +87,20 @@ const navigate = useNavigate();
     }
   }
 
+  const onIconChange = async(newIcon) => {
+    // memosの中身を直接変更したくないため避難
+    let temp = [...memos];
+    const index = temp.findIndex((e) => e._id === memoId);
+    temp[index] = {...temp[index], icon: newIcon};
+    setIcon(newIcon);
+    dispatch(setMemo(temp));
+    try {
+      await memoApi.update(memoId, {icon: newIcon});
+    } catch (err) {
+      alert(err);
+    }
+  };
+
   return (
     <>
       <Box 
@@ -106,31 +123,34 @@ const navigate = useNavigate();
         </IconButton>
       </Box>
       <Box sx={{padding: "10px 50px"}}>
-        <TextField 
-          onChange={updateTitle}
-          value={title}
-          placeholder="無題" 
-          variant="outlined" 
-          fullWidth 
-          sx={{
-            // 開発者ツールの要素タブでフォーカスした時のclassがMuiOutlinedInput-input
-            ".MuiOutlinedInput-input": {padding: 0},
-            ".MuiOutlinedInput-notchedOutline": {border: "none"},
-            ".MuiOutlinedInput-root": {fontSize: "2rem", fontWeight: "700"},
-          }} 
-        />
-        <TextField 
-          onChange={updateDescription}
-          value={description}
-          placeholder="追加" 
-          variant="outlined" 
-          fullWidth 
-          sx={{
-            ".MuiOutlinedInput-input": {padding: 0},
-            ".MuiOutlinedInput-notchedOutline": {border: "none"},
-            ".MuiOutlinedInput-root": {fontSize: "1rem"},
-          }}
-        />
+        <Box>
+          <EmojiPicker icon={icon} onChange={onIconChange} />
+          <TextField 
+            onChange={updateTitle}
+            value={title}
+            placeholder="無題" 
+            variant="outlined" 
+            fullWidth 
+            sx={{
+              // 開発者ツールの要素タブでフォーカスした時のclassがMuiOutlinedInput-input
+              ".MuiOutlinedInput-input": {padding: 0},
+              ".MuiOutlinedInput-notchedOutline": {border: "none"},
+              ".MuiOutlinedInput-root": {fontSize: "2rem", fontWeight: "700"},
+            }} 
+          />
+          <TextField 
+            onChange={updateDescription}
+            value={description}
+            placeholder="追加" 
+            variant="outlined" 
+            fullWidth 
+            sx={{
+              ".MuiOutlinedInput-input": {padding: 0},
+              ".MuiOutlinedInput-notchedOutline": {border: "none"},
+              ".MuiOutlinedInput-root": {fontSize: "1rem"},
+            }}
+          />
+        </Box>
       </Box>
     </>
   )
